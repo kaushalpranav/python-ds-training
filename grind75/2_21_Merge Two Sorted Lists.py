a6348313d918f2ba545ef5a3591a5a5cc8ade900
kaushalpranav <kaushalpranav_1996@yahoo.com>
2023-02-07 10:13:41 +0530

    feat: feb 7 LC code for merge sorted lists

--- grind75/2_21_Merge Two Sorted Lists.py
+++ grind75/2_21_Merge Two Sorted Lists.py
@@ -10,18 +10,54 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        pass
         """
-            1   ->   5   ->   7
-            2   ->   4   ->   6
-            
-            
-            while True:
-                if list1.val <= list2.val:
-                    result.next = list1
-                    list1 = list1.next
+        list1:    1   ->   5   ->   7   ->   8   ->  9
+        list2:    2   ->   4   ->   6
+
+        """
+
+        result = None
+        result_head = None
+
+        # what if one of the lists become empty
+        # maintain the result head somehow
+
+        while list1 is not None and list2 is not None:
+            if list1.val <= list2.val:
+                if result is None:
+                    result = ListNode(list1.val)
+                    result_head = result
                 else:
-                    result.next = list2
-                    list2 = list2.next
-        """
-        return None
+                    result.next = ListNode(list1.val)
+                    result = result.next
+                list1 = list1.next
+            else:
+                if result is None:
+                    result = ListNode(list2.val)
+                    result_head = result
+                else:
+                    result.next = ListNode(list2.val)
+                    result = result.next
+                list2 = list2.next
+
+        # either can be none
+
+        while list1 is not None:
+            if result is None:
+                result = ListNode(list1.val)
+                result_head = result
+            else:
+                result.next = ListNode(list1.val)
+                result = result.next
+            list1 = list1.next
+
+        while list2 is not None:
+            if result is None:
+                result = ListNode(list2.val)
+                result_head = result
+            else:
+                result.next = ListNode(list2.val)
+                result = result.next
+            list2 = list2.next
+
+        return result_head
